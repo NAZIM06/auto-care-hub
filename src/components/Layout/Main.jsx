0ac98a4dc54808b2db4ff4db05765ef405f903d1
unknown <anikm2982@gmail.com>
2023-05-10 12:10:40 +0600

    router setuped

---
+++ src/components/Layout/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Outlet } from 'react-router-dom';
+
+
+const Main = () => {
+    return (
+        <>
+            <div className="">
+                <Outlet></Outlet>
+                
+            </div>
+
+            
+
+        </>
+    );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import route from './components/Routes/Routes'
+// import AuthProvider from './components/Provider/AuthProvider'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <AuthProvider> </AuthProvider>
+    <RouterProvider router={route} />
+ 
 )
